b8a0a47cd2cd346e3edd447641b0951e182267ea
Gabriel Bueno <gabriels.bueno@outlook.com>
2020-09-09 16:05:05 -0300

    Master

---
+++ Interatividade.js
@@ -0,0 +1,21 @@
+var bom2 = 0;
+var medio2 = 0;
+var ruím2 = 0;
+
+function ruim () {
+    ruím2 += 1
+    document.getElementById("contagem1").innerHTML = ruím2;
+}
+
+function médio () {
+    medio2 += 1
+    document.getElementById("contagem2").innerHTML = medio2;
+}
+
+function bom() {
+    bom2 += 1
+    document.getElementById("contagem3").innerHTML = bom2;
+
+}
+
+
